5a60aab5784658fcdafb79edba18c156e6df6754
thenguyen123 <the78901234@gmail.com>
2023-03-14 21:11:03 +0700

    add

--- casestudy/case-study/src/app/component/contract/contract-edit/contract-edit.component.ts
+++ casestudy/case-study/src/app/component/contract/contract-edit/contract-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Contract} from '../../../model/contract/contract';
 
 @Component({
   selector: 'app-contract-edit',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-edit.component.css']
 })
 export class ContractEditComponent implements OnInit {
-
+ contractForm: FormGroup;
+contract: Contract;
   constructor() { }
 
   ngOnInit(): void {
+    this.contractForm = new FormGroup({
+      startDate: new FormControl(this.contract.startDate, [Validators.required]),
+      endDate: new FormControl(this.contract.endDate, [Validators.required]),
+      deposit: new FormControl(this.contract.deposit, [Validators.required, Validators.min(0)]),
+      customerTypes: new FormControl(this.contract.customerTypes.name, [Validators.required]),
+      facility: new FormControl(this.contract.facility.name, [Validators.required]),
+    });
   }
 
+  edit() {
+    
+  }
 }
